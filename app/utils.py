8f0ba84331356b65177f4760dcb8ce680b609deb
Luiz Felipe Soares <38364154+luizfsoares@users.noreply.github.com>
2021-07-26 19:29:53 -0300

    comentado

--- app/utils.py
+++ app/utils.py
@@ -1,19 +1,17 @@
-import pandas as pd
-
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
+"""  from transformers import GPT2Tokenizer, GPT2LMHeadModel
 from transformers import set_seed
 import random
-from re import sub
+from transformers import pipeline
 
 class Story_generator:
 
     def __init__(self):
-        self.model_path = "model_gpt2\checkpoint-53705"
+        self.model_path = "model\checkpoint-53705"
 
         set_seed(random.randint(0, 999))
 
         self.tokenizer = GPT2Tokenizer.from_pretrained(
-            'gpt2', bos_token='<|startoftext|>', eos_token='<|endoftext|>')
+            'gpt2', bos_token='<|startoftext|>', eos_token='<|endoftext|>', add_prefix_space = False)
 
         self.model = GPT2LMHeadModel.from_pretrained(
                     self.model_path, eos_token_id=self.tokenizer.eos_token_id, 
@@ -25,10 +23,10 @@ class Story_generator:
 
 
     def clean_text(self, text) -> str:
-        """
+
         Params: Input text
         Returns: treated input text (whithout spaces on the end, lower cased and with start token)
-        """
+
         restart = True
 
         while(restart):
@@ -43,45 +41,34 @@ class Story_generator:
 
 
     def generate_story(self, text, size, temperature) -> str:
-        """
+      
         Params: Input text, max size and temperature
         Returns: generated story
-        """
-        
-        text = self.clean_text(text)
+      
 
-        input_ids = self.tokenizer.encode(text, return_tensors='pt')
+        if text != "":
+            text = self.clean_text(text)
 
         input_length = len(text.split())
 
-        beam_outputs = self.model.generate(
-            input_ids, 
-            num_beams=5, 
-            max_length = input_length + size,
-            min_length = input_length + size - 10,
-            no_repeat_ngram_size=2, 
-            num_return_sequences=1,
-            reptition_penalty = float(1),
-            temperature = float(temperature),
-            early_stopping=True
-        )
+        writer = pipeline('text-generation', model = self.model, tokenizer = self.tokenizer)
 
-        story = self.tokenizer.decode(beam_outputs[0], skip_special_tokens=False)
+        story = writer(
+                text, max_length = input_length + size, 
+                temperature = float(temperature), 
+                repetition_penalty = float(1.2),
+                num_beams = 5,
+                no_repeat_ngram_size = 3)
+            
+        story = story[0].get('generated_text')
 
-        story = sub("<|startoftext|>", "", story)
-        story = sub("<|endoftext|>", "", story)
+        story = story.replace('<|startoftext|>', "")
+        story = story.replace('<|endoftext|>', "")
 
         return story
 
 story = Story_generator()
 
-example = story.generate_story("I was alone ", 100, 1.2 )
+example = story.generate_story("", 50, 1.2 )
 
-print(example)
-
-
-
-
-
-
-
+print("\n" +example+ "\n" """
