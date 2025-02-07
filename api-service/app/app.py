5d9e10d92b60c1ae186a4b97c3a05327ac574d4d
Arthur César <arthurcesat@gmail.com>
2025-02-07 16:33:03 -0300

    pequenos ajustes

--- api-service/app/app.py
+++ api-service/app/app.py
@@ -2,8 +2,12 @@ from flask import Flask
 from app.views import init_routes
 from app.metrics import start_metrics
 from app.logger import storia_logger
+from werkzeug.middleware.proxy_fix import ProxyFix  
 
 app = Flask(__name__, template_folder='templates')
+
+# Esses parâmetros indicam quantos valores de cada cabeçalho o ProxyFix deve confiar.
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1)
 
 # Inicializar rotas e métricas
 init_routes(app)
